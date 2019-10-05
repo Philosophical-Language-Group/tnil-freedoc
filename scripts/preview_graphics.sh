3fe498028b3f947668772f8789e95e9ec0e75f6b
porpoiseless <darkworldsawake@gmail.com>
2019-10-04 17:24:58 -0700

    Edit graphics script to output svgs

--- scripts/preview_graphics.sh
+++ scripts/preview_graphics.sh
@@ -2,15 +2,24 @@
 # convert files using inkscape
 input_dir=$1
 output_dir=$2
-
-function svg_to_png () {
+# produce png images
+function export_png() {
     path=$1
     echo $path
     file=${path##*/}
     base=${file%%.*}
     [ -e $path ] && inkscape $path -d 24 -e $output_dir/preview-${base}.png
 }
+# produce svg images
+function export_svg () {
+    path=$1
+    echo $path
+    file=${path##*/}
+    base=${file%%.*}
+    [ -e $path ] && inkscape $path -d 24 -l $output_dir/preview-${base}.svg
+}
 for f in $input_dir/*.svg
 do
-    svg_to_png $f
+    export_png $f
+    export_svg $f
 done
