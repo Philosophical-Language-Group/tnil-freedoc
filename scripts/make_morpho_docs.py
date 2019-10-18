6aa5ff2111ca2a0cde6d729a9d6b336902f7805f
porpoiseless <darkworldsawake@gmail.com>
2019-10-18 15:57:37 -0700

    Convert doc generation script to use template

--- scripts/make_morpho_docs.py
+++ scripts/make_morpho_docs.py
@@ -2,6 +2,7 @@
 
 # Import modules for dealing with the filesystem etc.
 from os import path
+from pathlib import Path
 import os
 import sys
 
@@ -11,6 +12,12 @@ try:
 except ModuleNotFoundError:
     print("Cannot run without 'yaml' package")
 
+# Check for jinja2 dependency.
+try:
+    from jinja2 import Environment, FileSystemLoader
+except ModuleNotFoundError:
+    print("Cannot run without 'jinja2' package")
+
 # Check that input & output directories were given.
 try:
     in_dir = sys.argv[1]
@@ -18,110 +25,15 @@ try:
 except IndexError:
     print("Supply an input directory and an output directory:")
     print(sys.argv[0] + " <in_dir> <out_dir>")
-
-# These are characters used to underline reST headings.
-HEADINGS = '=-^'
-
-# These are characters used to prefix for reST list items.
-BULLETS = '-+*'
-
-
-def cap(s):
-    """Capitalize the first letter of a string, preserving the rest.
-
-    Note that str.title() may be more appropriate here; e.g. "Case Scope" rather
-    than "Case scope". See
-    https://github.com/Ithkuilic-Language-Group/tnil-freedoc/issues/4.
-    """
-    return s[0].upper() + s[1:]
-
-
-def rst_heading(heading_level, s):
-    """Generate a reST heading.
-
-    The result consists of two lines and is terminated by a newline:
-    1. Capitalized version of the string (see cap())
-    2. String of the same length consisting of heading characters from HEADINGS;
-       the exact character depends on the heading level.
-    """
-    return cap(s) + "\n" + HEADINGS[heading_level - 1] * len(s)
-
-
-def rst_bullet(bullet_level, data):
-    """Generate a reST list item.
-
-    Indent to the appropriate bullet level and use a bullet character from
-    BULLETS depending on the level.
-    """
-    ret = "  " * (bullet_level - 1) + BULLETS[bullet_level - 1] + " "
-    link = f"`{cap(data['name'])}`_"
-    # link = f"{link_file}#{data['name'].replace(' ', '-')}"
-    # link_text = f"**{cap(data['name'])}**"
-    # if link:
-    #     ret += f"[{link_text}]({link})"
-    # else:
-    #     ret += link_text
-    ret += link
-    ret += " - "
-    ret += f"*{data['brief']}*"
-    ret += "\n"
-    return ret
-
-
-def rst_section(heading_level, data, *, include_heading=True):
-    """Make a section consisting of an item's heading (optional), description,
-    and enumeration of values (and groups, if applicable).
-    """
-    if include_heading:
-        ret = rst_heading(heading_level, data['name'])
-        ret += "\n\n"
-    else:
-        ret = ''
-    ret += data['full'].strip()
-    values = data.get('values')
-    groups = data.get('groups')
-    ret += "\n\n"
-    if values:
-        suffix = '' if data['name'].endswith('s') else 's'
-        ret += f"There are {len(values)} {cap(data['name'])}{suffix}"
-        if groups:
-            ret += f" split into {len(groups)} groups"
-        ret += ":"
-        ret += "\n\n"
-    return ret
-
-
-def rst_all(heading_level, data, **kwargs):
-    """Recursively generate nested reST sections.
-
-    Create a section for a category and a subsection for each value, or (if the
-    category has groups) a section for a category, a subsection for each group,
-    and a subsubsection for each value.
-    """
-    ret = rst_section(heading_level, data, **kwargs)
-    inners = None
-    values = data.get('values')
-    if values:
-        inners = values
-    groups = data.get('groups')
-    if groups:
-        values_dict = {}
-        for value in values:
-            values_dict[value['abbr']] = value
-        for group in groups:
-            group['name'] += f" {data['name']}s"
-            group['values'] = list(map(values_dict.get, group['members']))
-        inners = groups
-    if data.get('values_in_rst') == False:
-        inners = None
-    if inners:
-        for inner in inners:
-            ret += rst_bullet(1, inner)
-        ret += "\n"
-        for inner in inners:
-            ret += rst_all(heading_level + 1, inner)
-    return ret
-
+# look for a `templates` directory
+template_dir = os.getcwd() + "/templates"
+print("Template dir is: " + template_dir)
+# Set up template environment
+env = Environment(
+    loader=FileSystemLoader(template_dir)
+)
+#
+template = env.get_template('morpho.rst')
 
 # Iterate over files in input directory.
 for file in sorted(os.listdir(in_dir)):
@@ -146,12 +58,13 @@ for file in sorted(os.listdir(in_dir)):
         category = yaml.load(f, yaml.SafeLoader)
     # If the category has groups, indent one level less and exclude
     # top-level heading since this category will be in its own file.
-    if category.get('groups'):
-        heading_level = 1
-        include_heading = False
-    else:
-        heading_level = 2
-        include_heading = True
+    # if category.get('groups'):
+    #     heading_level = 1
+    #     include_heading = False
+    # else:
+    #     heading_level = 2
+    #     include_heading = True
     # Write the output.
     with open(out_file, 'w') as f:
-        f.write(rst_all(heading_level, category, include_heading=include_heading))
+        # f.write(rst_all(heading_level, category, include_heading=include_heading))
+        f.write(template.render(category))
